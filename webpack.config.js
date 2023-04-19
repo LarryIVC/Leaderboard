795702d23f1373a993746387e7337b7b8ed2e0f7
Larry Villegas <larry_villegas@hotmail.com>
2023-04-19 02:18:59 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ module.exports = {
   entry: {
     index: './src/index.js'
   },
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Leaderboard',
@@ -15,6 +18,9 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
