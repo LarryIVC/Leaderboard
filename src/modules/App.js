a8d486f4392ee3c558233a0673b44614d026952c
Larry Villegas <larry_villegas@hotmail.com>
2023-04-20 23:27:20 -0400

    Create App.js

---
+++ src/modules/App.js
@@ -0,0 +1,43 @@
+import { getGame, createScore } from './Api.js';
+
+// Display Scores
+const renderList = async () => {
+  const doc = document;
+  const ul = doc.getElementById('ul-scores');
+  ul.innerHTML = '';
+
+  const game = await getGame();
+  const { result } = game;
+
+  result.forEach((item) => {
+    const li = doc.createElement('li');
+    const spanName = doc.createElement('span');
+    const spanScore = doc.createElement('span');
+    const { user, score } = item;
+    spanName.textContent = `${user}:`;
+    spanScore.textContent = `${score}`;
+    li.appendChild(spanName);
+    li.appendChild(spanScore);
+    ul.appendChild(li);
+  });
+};
+
+// Add new score
+const addScores = () => {
+  const frmScores = document.getElementById('frm-add-score');
+  frmScores.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const output = await createScore(frmScores.name.value, frmScores.score.value);
+    renderList();
+    frmScores.name.value = '';
+    frmScores.score.value = '';
+    return output;
+  });
+};
+
+const App = () => {
+  addScores();
+  renderList();
+};
+
+export default App;
